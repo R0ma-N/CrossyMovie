b0d0b3d6b7ecc8741e9f0dd28a5a68b7f2d53dee
R0ma-N <x6wert@gmail.com>
2020-06-09 17:42:06 +0300

    Movie for Ilya

--- Assets/Scripts/TriggerSideJump.cs
+++ Assets/Scripts/TriggerSideJump.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class TriggerSideJump : MonoBehaviour
 {
+    public bool active;
+    Collider collider;
+
+    private void Awake()
+    {
+        collider = GetComponent<Collider>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         print(777);
         other.GetComponent<Box>().SideJump();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            active = true;
+            collider.enabled = true;
+        }
     }
 }
